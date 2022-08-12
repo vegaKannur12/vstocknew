c18b6791129ef445112865adc36bce5b5af1dcba
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2022-08-12 12:30:23 +0530

    fjiid

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
-import 'package:vstock/view/splashScreen.dart';
+// import 'package:vstock/view/splashScreen.dart';
 
-import 'package:vstock/screen/2_registration.dart';
-import 'package:vstock/screen/scan_type.dart';
+// import 'package:vstock/screen/2_registration.dart';
+// import 'package:vstock/screen/scan_type.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: SplashScreen(),
 
-      // theme: ThemeData(
-      //   scaffoldBackgroundColor: Colors.grey[200],
-      //   colorScheme: ColorScheme.fromSwatch().copyWith(
-      //     primary: Color(0xFF424242),
-      //     secondary: Color(0xFF424242),
-      //   ),
-      // ),
-      debugShowCheckedModeBanner: false,
-      home: ScanType(),
-    );
-  }
-}
+//       // theme: ThemeData(
+//       //   scaffoldBackgroundColor: Colors.grey[200],
+//       //   colorScheme: ColorScheme.fromSwatch().copyWith(
+//       //     primary: Color(0xFF424242),
+//       //     secondary: Color(0xFF424242),
+//       //   ),
+//       // ),
+//       debugShowCheckedModeBanner: false,
+//       home: ScanType(),
+//     );
+//   }
+// }
